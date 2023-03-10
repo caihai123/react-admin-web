d7e961995d2178e0b6f030dc3d14f4ad924e909d
caihai <ch15519060282@163.com>
2023-03-10 15:10:59 +0800

    【更新】添加401提示

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { message } from "antd";
 
 var instance = axios.create({
   baseURL:
@@ -8,7 +9,7 @@ var instance = axios.create({
 
 // 请求拦截器
 instance.interceptors.request.use(
-  function(config) {
+  function (config) {
     // 在发送请求之前做些什么
     let token = localStorage.getItem("token");
     if (token) {
@@ -17,7 +18,7 @@ instance.interceptors.request.use(
     config.headers.appCode = "ggfwpt2.0"; // 统一授权平台
     return config;
   },
-  function(error) {
+  function (error) {
     // 对请求错误做些什么
     return Promise.reject(error);
   }
@@ -25,7 +26,7 @@ instance.interceptors.request.use(
 
 // 响应拦截器
 instance.interceptors.response.use(
-  function(response) {
+  function (response) {
     // 对响应数据做点什么
     const data = response.data;
     var { code } = data;
@@ -34,12 +35,13 @@ instance.interceptors.response.use(
       case 200:
         return data;
       case 401: // 没有权限 包括未登录/登录超时
+        message.error("未登录或token过期，请重新登录！");
         return Promise.reject(data);
       default:
         return Promise.reject(data);
     }
   },
-  function(error) {
+  function (error) {
     // 对响应错误做点什么
     return Promise.reject(error);
   }
