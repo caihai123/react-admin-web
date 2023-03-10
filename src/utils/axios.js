845e12009b5f12cdb9dba38a0fa5c738f94b58bc
caihai <ch15519060282@163.com>
2023-03-10 12:38:38 +0800

    【更新】初始化axios

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -6,10 +6,15 @@ var instance = axios.create({
   timeout: 5000,
 });
 
-// 添加请求拦截器
+// 请求拦截器
 instance.interceptors.request.use(
   function(config) {
     // 在发送请求之前做些什么
+    let token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = "Bearer " + token;
+    }
+    config.headers.appCode = "ggfwpt2.0"; // 统一授权平台
     return config;
   },
   function(error) {
@@ -18,11 +23,21 @@ instance.interceptors.request.use(
   }
 );
 
-// 添加响应拦截器
+// 响应拦截器
 instance.interceptors.response.use(
   function(response) {
     // 对响应数据做点什么
-    return response;
+    const data = response.data;
+    var { code } = data;
+    // 请求成功时进行响应处理
+    switch (code) {
+      case 200:
+        return data;
+      case 401: // 没有权限 包括未登录/登录超时
+        return Promise.reject(data);
+      default:
+        return Promise.reject(data);
+    }
   },
   function(error) {
     // 对响应错误做点什么
