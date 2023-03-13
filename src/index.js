9ad53e501d144245b614be192cd63a084d2c7c12
caihai <ch15519060282@163.com>
2023-03-13 18:25:41 +0800

    【样式】格式化代码

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { ConfigProvider } from "antd";
+import { ConfigProvider, theme } from "antd";
 import zhCN from "antd/locale/zh_CN";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -16,6 +16,7 @@ root.render(
       token: {
         borderRadius: 2,
       },
+      algorithm: theme.darkAlgorithm,
     }}
   >
     <App />
