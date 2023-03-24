49a830c7900034afc670fdc048d9021bc7546769
caihai <ch15519060282@163.com>
2023-03-24 17:30:11 +0800

    【样式】更新登录页样式

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react";
-import { Button, Form, Input, theme } from "antd";
+import { Layout, Button, Form, Input, theme } from "antd";
 import { useNavigate } from "react-router-dom";
 import axios from "@/utils/axios";
+import LoginBanner from "@/components/LoginBanner";
 
 function Login() {
   // 验证码相关 start
@@ -39,54 +40,76 @@ function Login() {
   } = theme.useToken();
 
   return (
-    <div
+    <Layout
       style={{
         height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
         background: colorBgContainer,
       }}
     >
-      <Form
-        onFinish={submitForm}
-        layout="vertical"
-        size="large"
-        style={{ width: 600 }}
-      >
-        <Form.Item
-          label="用户名"
-          name="userAccount"
-          rules={[{ required: true, message: "请填写用户名!" }]}
+      <Layout.Header>
+        <h1 style={{ fontSize: 24, color: "#fff" }}>公共服务管理平台</h1>
+      </Layout.Header>
+      <Layout>
+        <Layout.Sider theme="light" width="60%" style={{ padding: 24 }}>
+          <LoginBanner />
+        </Layout.Sider>
+        <Layout.Content
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
         >
-          <Input placeholder="请输入用户名" />
-        </Form.Item>
-        <Form.Item
-          label="密码"
-          name="userPassword"
-          rules={[{ required: true, message: "请填写密码!" }]}
-        >
-          <Input placeholder="请输入密码" />
-        </Form.Item>
+          <Form
+            onFinish={submitForm}
+            layout="vertical"
+            size="large"
+            style={{ width: 400 }}
+          >
+            <h1 style={{ fontSize: 24 }}>欢迎登录</h1>
+            <Form.Item
+              label="用户名"
+              name="userAccount"
+              rules={[{ required: true, message: "请填写用户名!" }]}
+              required={false}
+            >
+              <Input placeholder="请输入用户名" />
+            </Form.Item>
+            <Form.Item
+              label="密码"
+              name="userPassword"
+              rules={[{ required: true, message: "请填写密码!" }]}
+              required={false}
+            >
+              <Input.Password placeholder="请输入密码" />
+            </Form.Item>
 
-        <Form.Item
-          label="验证码"
-          name="verifyCode"
-          rules={[{ required: true, message: "请填写验证码!" }]}
-        >
-          <Input placeholder="请输入验证码" />
-        </Form.Item>
+            <Form.Item
+              label="验证码"
+              name="verifyCode"
+              rules={[{ required: true, message: "请填写验证码!" }]}
+              required={false}
+            >
+              <Input.Search
+                placeholder="请输入验证码"
+                enterButton={authCode[0]}
+                onSearch={getAuthCode}
+              />
+            </Form.Item>
 
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-            登 录
-          </Button>
-          <Button style={{ width: 70 }} onClick={getAuthCode}>
-            {authCode[0]}
-          </Button>
-        </Form.Item>
-      </Form>
-    </div>
+            <Form.Item style={{ marginTop: 40 }}>
+              <Button
+                type="primary"
+                htmlType="submit"
+                style={{ width: "100%" }}
+              >
+                登 录
+              </Button>
+            </Form.Item>
+          </Form>
+        </Layout.Content>
+      </Layout>
+    </Layout>
   );
 }
 
