a1272f86dfa95cd30e2a218a0095be030daf238d
caihai <ch15519060282@163.com>
2023-03-26 19:16:04 +0800

    【样式】优化401 403页面样式

--- src/pages/401.js
+++ src/pages/401.js
@@ -4,15 +4,24 @@ import { useNavigate } from "react-router-dom";
 export default function Page() {
   let navigate = useNavigate();
   return (
-    <Result
-      status="403"
-      title="401"
-      subTitle="对不起，您没有访问本页的权限。"
-      extra={
-        <Button type="primary" onClick={() => navigate("/")}>
-          回到首页
-        </Button>
-      }
-    />
+    <div
+      style={{
+        height: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Result
+        status="403"
+        title="401"
+        subTitle="对不起，您没有访问本页的权限。"
+        extra={
+          <Button type="primary" onClick={() => navigate("/")}>
+            回到首页
+          </Button>
+        }
+      />
+    </div>
   );
 }
--- src/pages/404.js
+++ src/pages/404.js
@@ -5,15 +5,24 @@ export default function Page() {
   let navigate = useNavigate();
 
   return (
-    <Result
-      status="404"
-      title="404"
-      subTitle="对不起，您访问的页面不存在。"
-      extra={
-        <Button type="primary" onClick={() => navigate("/")}>
-          回到首页
-        </Button>
-      }
-    />
+    <div
+      style={{
+        height: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Result
+        status="404"
+        title="404"
+        subTitle="对不起，您访问的页面不存在。"
+        extra={
+          <Button type="primary" onClick={() => navigate("/")}>
+            回到首页
+          </Button>
+        }
+      />
+    </div>
   );
 }
