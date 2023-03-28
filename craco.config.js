736c1615ede95dc0fee7d81a228184229c7547bc
caihai <ch15519060282@163.com>
2023-03-28 17:40:23 +0800

    【更新】添加开发启动进度条

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const WebpackBar = require("webpackbar");
 
 module.exports = {
   devServer: {
@@ -19,5 +20,6 @@ module.exports = {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
+    plugins: [new WebpackBar()],
   },
 };
