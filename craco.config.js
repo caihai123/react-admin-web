5ee1a256df933fb0c2e36b34c61af34b105d6b4e
caihai <ch15519060282@163.com>
2023-03-28 17:41:09 +0800

    【更新】启动之后不自动打开浏览器

--- craco.config.js
+++ craco.config.js
@@ -4,7 +4,7 @@ const WebpackBar = require("webpackbar");
 module.exports = {
   devServer: {
     port: 1234,
-    // open: 'Chrome'//构建完成之后自动打开谷歌
+    open: false, //构建完成之后自动打开谷歌
     proxy: {
       "/api": {
         target: "https://test-portal.gshbzw.com",
