07df8f8d8bd0074af0bca61ae09f468a6bd564e8
caihai <ch15519060282@163.com>
2023-04-03 10:35:50 +0800

    【优化】代码优化

--- src/console.js
+++ src/console.js
@@ -4,19 +4,13 @@ const { REACT_APP_Commit_Hash, REACT_APP_Build_Date } = process.env;
 
 const consoleFn = function (label, val) {
   const style1 =
-    "background:#35495e ; padding: 1px; border-radius: 3px 0 0 3px;  color: #fff";
+    "padding:1px; border-radius:3px 0 0 3px; color:#fff; background:#35495e;";
   const style2 =
-    "background:#41b883 ; padding: 1px; border-radius: 0 3px 3px 0;  color: #fff";
-  const style3 = "background:transparent";
+    "padding:1px; border-radius:0 3px 3px 0; color:#fff; background:#41b883;";
+
   window.setTimeout(
     // eslint-disable-next-line no-console
-    console.log.bind(
-      console,
-      `%c ${label} %c ${val} %c`,
-      style1,
-      style2,
-      style3
-    )
+    console.log.bind(console, `%c ${label} %c ${val} `, style1, style2)
   );
 };
 
