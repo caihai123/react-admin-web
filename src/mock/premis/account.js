39f658d589929dae1e996bea1d427c1a5a95e11c
caihai <ch15519060282@163.com>
2023-04-08 23:05:56 +0800

    【优化】重构mockjs代码

--- src/mock/premis/account.js
+++ src/mock/premis/account.js
@@ -1,25 +1,34 @@
 import Mock from "mockjs";
 
-Mock.mock("/api/account/page", "post", function ({ body }) {
-  const { pageIndex, pageSize = 10 } = JSON.parse(body);
+const mock = [
+  {
+    // 用户列表分页
+    url: "/api/account/page",
+    type: "post",
+    handler({ body }) {
+      const { pageIndex, pageSize = 10 } = JSON.parse(body);
 
-  return {
-    result: {
-      records: [...Array(pageSize)].map(() =>
-        Mock.mock({
-          id: "@guid",
-          account: /^[a-zA-Z0-9_-]{4,16}$/,
-          name: "@cname",
-          "gender|1": [1, 2],
-          phone: /^(?:(?:\+|00)86)?1[3-9]\d{9}$/,
-          email: "@email",
-          "status|1": [0, 1],
-        })
-      ),
-      total: 100,
-      pageIndex,
+      return {
+        result: {
+          records: [...Array(pageSize)].map(() =>
+            Mock.mock({
+              id: "@guid",
+              account: /^[a-zA-Z0-9_-]{4,16}$/,
+              name: "@cname",
+              "gender|1": [1, 2],
+              phone: /^(?:(?:\+|00)86)?1[3-9]\d{9}$/,
+              email: "@email",
+              "status|1": [0, 1],
+            })
+          ),
+          total: 100,
+          pageIndex,
+        },
+        status: "success",
+        msg: "成功！",
+      };
     },
-    status: "success",
-    msg: "成功！",
-  };
-});
+  },
+];
+
+export default mock;
--- src/mock/premis/role.js
+++ src/mock/premis/role.js
@@ -1,22 +1,30 @@
 import Mock from "mockjs";
 
-Mock.mock("/api/role/page", "post", function ({ body }) {
-  const { pageIndex, pageSize = 10 } = JSON.parse(body);
+const mock = [
+  {
+    url: "/api/role/page",
+    type: "post",
+    handler({ body }) {
+      const { pageIndex, pageSize = 10 } = JSON.parse(body);
 
-  return {
-    result: {
-      records: [...Array(pageSize)].map(() =>
-        Mock.mock({
-          id: "@guid",
-          roleName: "@ctitle",
-          description: "@csentence",
-          "status|1": [0, 1],
-        })
-      ),
-      total: 100,
-      pageIndex,
+      return {
+        result: {
+          records: [...Array(pageSize)].map(() =>
+            Mock.mock({
+              id: "@guid",
+              roleName: "@ctitle",
+              description: "@csentence",
+              "status|1": [0, 1],
+            })
+          ),
+          total: 100,
+          pageIndex,
+        },
+        status: "success",
+        msg: "成功！",
+      };
     },
-    status: "success",
-    msg: "成功！",
-  };
-});
+  },
+];
+
+export default mock;
