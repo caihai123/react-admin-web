a758ed0b5ba257ad555281563ff9c801fe0bffb8
caihai <ch15519060282@163.com>
2023-04-09 00:13:51 +0800

    【样式】调整登录页样式

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,11 +1,29 @@
 import { useState } from "react";
-import { App, Layout, Button, Form, Input, theme, Switch } from "antd";
+import {
+  App,
+  Layout,
+  Button,
+  Form,
+  Input,
+  theme,
+  Switch,
+  Checkbox,
+} from "antd";
 import { useNavigate } from "react-router-dom";
 import LoginBanner from "./components/LoginBanner";
 import { useSelector, useDispatch } from "react-redux";
 import { selectTheme, setTheme } from "@/store/modules/system";
 import styled from "styled-components";
 import useAxios from "@/hooks/axios";
+import {
+  UserOutlined,
+  LockOutlined,
+  AlipayCircleOutlined,
+  TaobaoCircleOutlined,
+  WeiboOutlined,
+} from "@ant-design/icons";
+import LogoSvg from "@/assets/logo.svg";
+import FormBg from "@/assets/login-bg.svg";
 
 const Header = styled(Layout.Header)`
   display: flex;
@@ -15,6 +33,30 @@ const Header = styled(Layout.Header)`
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.15);
   position: relative;
   z-index: 9;
+`;
+
+const Title = styled.div`
+  display: flex;
+  align-items: center;
+  & > .logo {
+    height: 44px;
+  }
+  & > h1 {
+    margin: 0;
+    margin-left: 12px;
+  }
+`;
+
+const BlogrollIcon = styled.span`
+  margin-left: 8px;
+  color: rgba(0, 0, 0, 0.2);
+  font-size: 24px;
+  vertical-align: middle;
+  cursor: pointer;
+  transition: color 0.3s ease 0s;
+  &:hover {
+    color: rgb(24, 144, 255);
+  }
 `;
 
 const Login = function () {
@@ -52,7 +94,10 @@ const Login = function () {
   return (
     <Layout style={{ minHeight: "100vh" }}>
       <Header background={colorBgContainer}>
-        <h1 style={{ fontSize: 24 }}>公共服务管理平台</h1>
+        <Title>
+          <img src={LogoSvg} alt="logo" className="logo" />
+          <h1 style={{ fontSize: 24 }}>{process.env.REACT_APP_WEBSITE_NAME}</h1>
+        </Title>
         <div className="tools">
           <Switch
             checked={themeName !== "dark"}
@@ -93,33 +138,50 @@ const Login = function () {
             justifyContent: "center",
             alignItems: "center",
             background: colorBgLayout,
+            backgroundImage: `url(${FormBg})`,
           }}
         >
           <Form
             onFinish={submitForm}
             layout="vertical"
             size="large"
-            style={{ width: "100%", maxWidth: 448, padding: 24 }}
+            style={{
+              width: "100%",
+              marginTop: -40,
+              maxWidth: 448,
+              padding: 24,
+            }}
+            initialValues={{
+              userAccount: "admin",
+              userPassword: "password",
+            }}
           >
-            <h1 style={{ fontSize: 24 }}>欢迎登录</h1>
+            <h1 style={{ marginBottom: 24, fontSize: 24 }}>欢迎登录</h1>
             <Form.Item
-              label="用户名"
               name="userAccount"
               rules={[{ required: true, message: "请填写用户名!" }]}
               required={false}
             >
-              <Input placeholder="用户名：admin" />
+              <Input prefix={<UserOutlined />} placeholder="用户名：admin" />
             </Form.Item>
             <Form.Item
-              label="密码"
               name="userPassword"
               rules={[{ required: true, message: "请填写密码!" }]}
               required={false}
             >
-              <Input.Password placeholder="密码：password" />
+              <Input.Password
+                prefix={<LockOutlined />}
+                placeholder="密码：password"
+              />
             </Form.Item>
 
-            <Form.Item style={{ marginTop: 40 }}>
+            <div style={{ marginBottom: 24 }}>
+              <Checkbox defaultChecked>自动登录</Checkbox>
+              {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+              <a style={{ float: "right" }}>忘记密码？</a>
+            </div>
+
+            <Form.Item>
               <Button
                 type="primary"
                 htmlType="submit"
@@ -129,6 +191,19 @@ const Login = function () {
                 登 录
               </Button>
             </Form.Item>
+
+            <div>
+              其他登录方式：
+              <BlogrollIcon>
+                <AlipayCircleOutlined />
+              </BlogrollIcon>
+              <BlogrollIcon>
+                <TaobaoCircleOutlined />
+              </BlogrollIcon>
+              <BlogrollIcon>
+                <WeiboOutlined />
+              </BlogrollIcon>
+            </div>
           </Form>
         </Layout.Content>
       </Layout>
