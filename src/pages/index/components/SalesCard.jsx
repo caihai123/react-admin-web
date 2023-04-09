77e4522ef300d99c78ae7fb4ea418e8332099caf
caihai <ch15519060282@163.com>
2023-04-09 22:48:25 +0800

    【新增】排名统计

--- src/pages/index/components/SalesCard.jsx
+++ src/pages/index/components/SalesCard.jsx
@@ -61,6 +61,58 @@ const salesData = [
   },
 ];
 
+const rankingListData = [...Array(7)].map((_, i) => ({
+  title: `工专路 ${i} 号店`,
+  total: 323234,
+}));
+
+const StyledSalesRank = styled.div`
+  margin-bottom: 24px;
+  padding: 0 32px;
+  & > .rankingList {
+    margin: 25px 0 0;
+    padding: 0;
+    list-style: none;
+    & > li {
+      display: flex;
+      align-items: center;
+      margin-top: 16px;
+      zoom: 1;
+    }
+    & .rankingItemNumber {
+      display: inline-block;
+      width: 20px;
+      height: 20px;
+      margin-top: 1.5px;
+      margin-right: 16px;
+      font-weight: 600;
+      font-size: 12px;
+      line-height: 20px;
+      text-align: center;
+      background-color: #fafafa;
+      border-radius: 20px;
+      &.active {
+        color: #fff;
+        background-color: #314659;
+      }
+    }
+
+    & .rankingItemTitle {
+      flex: 1 1;
+      margin-right: 8px;
+      overflow: hidden;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+    }
+
+    & span {
+      color: rgba(0, 0, 0, 0.85);
+      font-size: 14px;
+      line-height: 22px;
+    }
+  }
+`;
+
 const SalesCard = function () {
   return (
     <Card bordered={false} bodyStyle={{ padding: 0 }}>
@@ -82,7 +134,14 @@ const SalesCard = function () {
             key: "sales",
             children: (
               <Row>
-                <Col xl={16} lg={12} md={12} sm={24} xs={24}>
+                <Col
+                  xl={16}
+                  lg={12}
+                  md={12}
+                  sm={24}
+                  xs={24}
+                  style={{ marginBottom: 24 }}
+                >
                   <Column
                     height={300}
                     data={salesData}
@@ -98,10 +157,31 @@ const SalesCard = function () {
                       },
                     }}
                     meta={{ y: { alias: "销售量" } }}
-                    padding={[16, 48, 48, 48]}
+                    padding={[16, 48, 24, 48]}
                   />
                 </Col>
-                <Col xl={8} lg={12} md={12} sm={24} xs={24}></Col>
+                <Col xl={8} lg={12} md={12} sm={24} xs={24}>
+                  <StyledSalesRank>
+                    <h4>门店销售额排名</h4>
+                    <ul className="rankingList">
+                      {rankingListData.map((item, i) => (
+                        <li key={item.title}>
+                          <span
+                            className={`rankingItemNumber ${
+                              i < 3 ? "active" : ""
+                            }`}
+                          >
+                            {i + 1}
+                          </span>
+                          <span className="rankingItemTitle" title={item.title}>
+                            {item.title}
+                          </span>
+                          <span className="rankingItemValue">{item.total}</span>
+                        </li>
+                      ))}
+                    </ul>
+                  </StyledSalesRank>
+                </Col>
               </Row>
             ),
           },
@@ -110,7 +190,14 @@ const SalesCard = function () {
             key: "views",
             children: (
               <Row>
-                <Col xl={16} lg={12} md={12} sm={24} xs={24}>
+                <Col
+                  xl={16}
+                  lg={12}
+                  md={12}
+                  sm={24}
+                  xs={24}
+                  style={{ marginBottom: 24 }}
+                >
                   <Column
                     height={300}
                     data={salesData}
@@ -126,10 +213,31 @@ const SalesCard = function () {
                       },
                     }}
                     meta={{ y: { alias: "销售量" } }}
-                    padding={[16, 48, 48, 48]}
+                    padding={[16, 48, 24, 48]}
                   />
                 </Col>
-                <Col xl={8} lg={12} md={12} sm={24} xs={24}></Col>
+                <Col xl={8} lg={12} md={12} sm={24} xs={24}>
+                  <StyledSalesRank>
+                    <h4>门店访问量排名</h4>
+                    <ul className="rankingList">
+                      {rankingListData.map((item, i) => (
+                        <li key={item.title}>
+                          <span
+                            className={`rankingItemNumber ${
+                              i < 3 ? "active" : ""
+                            }`}
+                          >
+                            {i + 1}
+                          </span>
+                          <span className="rankingItemTitle" title={item.title}>
+                            {item.title}
+                          </span>
+                          <span className="rankingItemValue">{item.total}</span>
+                        </li>
+                      ))}
+                    </ul>
+                  </StyledSalesRank>
+                </Col>
               </Row>
             ),
           },
