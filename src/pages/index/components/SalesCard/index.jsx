6c12afb706ab57e55adbbc32e6e2ba02488ec28e
caihai <ch15519060282@163.com>
2023-04-10 21:40:36 +0800

    【优化】普通的class可能被覆盖，采用更安全的module css

--- src/pages/index/components/SalesCard/index.jsx
+++ src/pages/index/components/SalesCard/index.jsx
@@ -1,14 +1,6 @@
 import { Card, Tabs, DatePicker, Space, Row, Col } from "antd";
-import styled from "styled-components";
 import { Column } from "@ant-design/plots";
-
-const SalesExtra = styled.div`
-  display: inline-block;
-  margin-right: 24px;
-  & > a {
-    margin-left: 24px;
-  }
-`;
+import styles from "./style.module.css";
 
 const salesData = [
   {
@@ -68,64 +60,18 @@ const rankingListData = [...Array(7)].map((_, i) => ({
 
 const formatNumber = (num) => parseFloat(num).toLocaleString("en-US");
 
-const StyledSalesRank = styled.div`
-  margin-bottom: 24px;
-  padding: 0 32px;
-  & > .rankingList {
-    margin: 25px 0 0;
-    padding: 0;
-    list-style: none;
-    & > li {
-      display: flex;
-      align-items: center;
-      margin-top: 16px;
-      zoom: 1;
-    }
-    & .rankingItemNumber {
-      display: inline-block;
-      width: 20px;
-      height: 20px;
-      margin-top: 1.5px;
-      margin-right: 16px;
-      font-weight: 600;
-      font-size: 12px;
-      line-height: 20px;
-      text-align: center;
-      background-color: #fafafa;
-      border-radius: 20px;
-      &.active {
-        color: #fff;
-        background-color: #314659;
-      }
-    }
-
-    & .rankingItemTitle {
-      flex: 1 1;
-      margin-right: 8px;
-      overflow: hidden;
-      white-space: nowrap;
-      text-overflow: ellipsis;
-    }
-
-    & span {
-      font-size: 14px;
-      line-height: 22px;
-    }
-  }
-`;
-
 const SalesCard = function () {
   return (
     <Card bordered={false} bodyStyle={{ padding: 0 }}>
       <Tabs
         tabBarExtraContent={
           <Space style={{ paddingRight: 16 }}>
-            <SalesExtra>
+            <div className={styles.salesExtra}>
               <a>今日</a>
               <a>本周</a>
               <a>本月</a>
               <a>本年</a>
-            </SalesExtra>
+            </div>
             <DatePicker.RangePicker style={{ width: 256 }} />
           </Space>
         }
@@ -162,26 +108,31 @@ const SalesCard = function () {
                   />
                 </Col>
                 <Col xl={8} lg={12} md={12} sm={24} xs={24}>
-                  <StyledSalesRank>
-                    <h4>门店销售额排名</h4>
-                    <ul className="rankingList">
+                  <div className={styles.salesRank}>
+                    <h4 className={styles.rankingTitle}>门店访问量排名</h4>
+                    <ul className={styles.rankingList}>
                       {rankingListData.map((item, i) => (
                         <li key={item.title}>
                           <span
-                            className={`rankingItemNumber ${
-                              i < 3 ? "active" : ""
+                            className={`${styles.rankingItemNumber} ${
+                              i < 3 ? styles.active : ""
                             }`}
                           >
                             {i + 1}
                           </span>
-                          <span className="rankingItemTitle" title={item.title}>
+                          <span
+                            className={styles.rankingItemTitle}
+                            title={item.title}
+                          >
                             {item.title}
                           </span>
-                          <span>{formatNumber(item.total)}</span>
+                          <span className={styles.rankingItemValue}>
+                            {formatNumber(item.total)}
+                          </span>
                         </li>
                       ))}
                     </ul>
-                  </StyledSalesRank>
+                  </div>
                 </Col>
               </Row>
             ),
@@ -218,26 +169,31 @@ const SalesCard = function () {
                   />
                 </Col>
                 <Col xl={8} lg={12} md={12} sm={24} xs={24}>
-                  <StyledSalesRank>
-                    <h4>门店访问量排名</h4>
-                    <ul className="rankingList">
+                  <div className={styles.salesRank}>
+                    <h4 className={styles.rankingTitle}>门店访问量排名</h4>
+                    <ul className={styles.rankingList}>
                       {rankingListData.map((item, i) => (
                         <li key={item.title}>
                           <span
-                            className={`rankingItemNumber ${
-                              i < 3 ? "active" : ""
+                            className={`${styles.rankingItemNumber} ${
+                              i < 3 ? styles.active : ""
                             }`}
                           >
                             {i + 1}
                           </span>
-                          <span className="rankingItemTitle" title={item.title}>
+                          <span
+                            className={styles.rankingItemTitle}
+                            title={item.title}
+                          >
                             {item.title}
                           </span>
-                          <span>{formatNumber(item.total)}</span>
+                          <span className={styles.rankingItemValue}>
+                            {formatNumber(item.total)}
+                          </span>
                         </li>
                       ))}
                     </ul>
-                  </StyledSalesRank>
+                  </div>
                 </Col>
               </Row>
             ),
