7c82a50b115db2e304804ef0a6a37ce358746656
caihai <ch15519060282@163.com>
2023-05-10 16:15:58 +0800

    【更新】开发环境启动之后打印信息到终端

--- scripts/start.js
+++ scripts/start.js
@@ -1,22 +1,32 @@
+/* eslint-disable no-console */
 const webpack = require("webpack");
 const WebpackDevServer = require("webpack-dev-server");
 const devServerConfig = require("../config/devServer");
 const configFactory = require("../config/webpack.config");
-const clearConsole = require("react-dev-utils/clearConsole");
+// const clearConsole = require("react-dev-utils/clearConsole");
 const chalk = require("react-dev-utils/chalk");
+const { prepareUrls } = require("react-dev-utils/WebpackDevServerUtils");
 
 process.env.NODE_ENV = "development";
 
-const isInteractive = process.stdout.isTTY;
-
 const webpackConfig = configFactory(process.env.NODE_ENV);
 const compiler = webpack(webpackConfig);
+
+compiler.hooks.done.tap("my-serve", (stats) => {
+  if (stats.hasErrors()) {
+    return;
+  }
+  const urls = prepareUrls("http", "0.0.0.0", devServerConfig.port, "/");
+  console.log();
+  console.log(`  App running at:`);
+  console.log(`  - Local:   ${chalk.cyan(urls.localUrlForBrowser)}`);
+  console.log(`  - Network: `);
+  console.log();
+  console.log("  Note that the development build is not optimized.");
+  console.log(
+    `  To create a production build, run ${chalk.cyan("npm run build")}.`
+  );
+});
 const devServer = new WebpackDevServer(devServerConfig, compiler);
 
-devServer.startCallback(() => {
-  if (isInteractive) {
-    clearConsole();
-  }
-  // eslint-disable-next-line no-console
-  console.log(chalk.cyan("启动开发服务器...\n"));
-});
+devServer.start();
