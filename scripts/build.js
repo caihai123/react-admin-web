c2a41621707c0880362eccba4d4b645d28b5ac59
caihai <ch15519060282@163.com>
2023-06-20 14:27:58 +0800

    【更新】添加打包提示

--- scripts/build.js
+++ scripts/build.js
@@ -1,7 +1,20 @@
+/* eslint-disable no-console */
 const webpack = require("webpack");
 const configFactory = require("../config/webpack.config");
+const chalk = require("react-dev-utils/chalk");
 
 process.env.NODE_ENV = "production";
 const webpackConfig = configFactory(process.env.NODE_ENV);
 
-webpack(webpackConfig).run(() => {});
+const build = function () {
+  console.log(
+    chalk.bgBlue.black(" INFO ") + chalk.cyan(" 生产代码打包中，请等待...\n")
+  );
+  webpack(webpackConfig).run((err, stats) => {
+    console.log(
+      chalk.bgBlue.black(" INFO ") + chalk.cyan(" 打包成功，dist文件已准备好！")
+    );
+  });
+};
+
+build();
