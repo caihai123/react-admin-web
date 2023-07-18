cf3a85e9d840c41ffbff7864e0e9fd0685ff81fa
caihai <ch15519060282@163.com>
2023-07-18 17:25:42 +0800

    【优化】处理刷新时出现404的bug

--- src/layout/LayContent.js
+++ src/layout/LayContent.js
@@ -1,7 +1,6 @@
 import { Suspense, useMemo } from "react";
 import { Layout, Spin } from "antd";
 import { Routes, Route } from "react-router-dom";
-import { useBoolean, useTimeout } from "ahooks";
 import Redirect from "./redirect";
 import Error404 from "@/pages/404";
 import Error401 from "@/pages/401";
@@ -32,10 +31,6 @@ export default function LayContent({ initialMenuList, loading }) {
     [initialMenuList]
   );
 
-  const [pageloading, { set: setPageloading }] = useBoolean(true);
-  // 首次加载时可能会先显示出401页面，所以需要控制让路由延时渲染，留出计算菜单权限的时间
-  useTimeout(() => setPageloading(false), 500);
-
   // 判断当前路由是否在菜单列表中 如果在返回true 否则返回false
   const isInMenuList = (pathname) => {
     return menuList.some((item) => item.path === pathname);
@@ -43,7 +38,7 @@ export default function LayContent({ initialMenuList, loading }) {
 
   return (
     <Content style={{ margin: 20 }}>
-      {!pageloading ? (
+      {!loading ? (
         <Suspense fallback={<PageLoading />}>
           <Routes>
             {routes.map((item) => (
--- src/layout/index.js
+++ src/layout/index.js
@@ -70,7 +70,7 @@ export default function LayoutViwe() {
   const [collapsed, setCollapsed] = useState(false); // 控制侧边栏展开收起
 
   const [initialMenuList, setInitialMenuList] = useState([]); // 后端返回的路由表
-  const [menuLoading, setMenuLoading] = useState(false);
+  const [menuLoading, setMenuLoading] = useState(true);
   useMount(() => {
     // 获取权限路由列表
     setMenuLoading(true);
