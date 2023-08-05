252d34eea4fbbaf85494d7895d2091677e1019fe
caihai <ch15519060282@163.com>
2023-08-05 18:09:08 +0800

    【文档】完善注释

--- config/env.js
+++ config/env.js
@@ -2,7 +2,7 @@ module.exports = {
   // 应用名称
   APP_NAME: "React Or Antd",
 
-  //部署应用包时的基本 URL
+  //部署应用包时的基本 URL，只支持绝对路径，我没有做容错处理，需要严格保持以'/'开头和结尾，一般情况下设置为'/'。
   PUBLIC_PATH: "/react-admin-web/",
 
   // 是否开启 mockapi
