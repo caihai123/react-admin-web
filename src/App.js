1f711f90510d09ee9d1678f671ae7303e3b46aee
caihai <ch15519060282@163.com>
2023-08-08 11:28:49 +0800

    【修复】处理antd的DatePicker国际化失效的问题

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import zhCN from "antd/locale/zh_CN";
 import { selectTheme } from "@/store//modules//system";
 import { useSelector } from "react-redux";
 
+// antd 的 DatePicker 国际化失效，官网说是因为我项目中同时存在两个dayjs，也确实是这样，ahooks中也有dayjs
+// 我这样加载中文包之后就好了，我猜测应该是antd设置dayjs.locale的时候没有找到正确的中文包
+import "dayjs/locale/zh-cn";
+
 export default function App() {
   const themeName = useSelector(selectTheme);
 
