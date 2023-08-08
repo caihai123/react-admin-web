62411d1526e0e0faaadd6b0fc48cb3645c90305b
caihai <ch15519060282@163.com>
2023-08-08 11:12:20 +0800

    【优化】优化日期相关

--- src/components/ProTable/FormItem/DateRange.jsx
+++ src/components/ProTable/FormItem/DateRange.jsx
@@ -1,9 +1,11 @@
 import { DatePicker, Form } from "antd";
 
+const { RangePicker } = DatePicker;
+
 export default function Date({ item }) {
   return (
     <Form.Item label={item.title} name={item.dataIndex} {...item.formItemProps}>
-      <DatePicker {...item.fieldProps} />
+      <RangePicker {...item.fieldProps} />
     </Form.Item>
   );
 }
--- src/components/ProTable/FormItem/index.jsx
+++ src/components/ProTable/FormItem/index.jsx
@@ -1,19 +1,19 @@
 import Input from "./Input";
 import Select from "./Select";
 import Date from "./Date";
-import DateTime from "./DateTime";
+import DateRange from "./DateRange";
 
 export default function FormItem({ item }) {
   if (item.renderFormItem) {
     return item.renderFormItem();
   } else {
-    switch (item.valueType) {
+    switch (item.type) {
       case "select":
         return <Select item={item} />;
       case "date":
         return <Date item={item} />;
-      case "dateTime":
-        return <DateTime item={item} />;
+      case "dateRange":
+        return <DateRange item={item} />;
       default:
         return <Input item={item} />;
     }
