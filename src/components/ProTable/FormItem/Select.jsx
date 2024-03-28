7233290c21b9797e6b54aec16666dc76ae9de486
caihai <ch15519060282@163.com>
2024-03-29 02:36:34 +0800

    【更新】根据color渲染option

--- src/components/ProTable/FormItem/Select.jsx
+++ src/components/ProTable/FormItem/Select.jsx
@@ -1,15 +1,24 @@
-import { Select as ASelect, Form } from "antd";
+import { Select as ASelect, Form, Badge } from "antd";
 
 export default function Select({ item }) {
   return (
     <Form.Item label={item.title} name={item.dataIndex} {...item.formItemProps}>
       <ASelect
         placeholder={`请选择${item.title}`}
-        options={item.options}
         style={{ width: 183 }}
         allowClear
         {...item.fieldProps}
-      />
+      >
+        {item.options.map((item) => (
+          <ASelect.Option key={item.value} value={item.value}>
+            {item.color ? (
+              <Badge color={item.color} text={item.label} />
+            ) : (
+              item.label
+            )}
+          </ASelect.Option>
+        ))}
+      </ASelect>
     </Form.Item>
   );
 }
