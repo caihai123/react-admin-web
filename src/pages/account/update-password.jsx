46af908b58fcf3e166b0ef8276fe09402a28852a
caihai <ch15519060282@163.com>
2024-05-02 03:58:05 +0800

    【更新】修改密码页面

--- src/pages/account/update-password.jsx
+++ src/pages/account/update-password.jsx
@@ -1,3 +1,43 @@
+import { Card, Form, Input, Button } from "antd";
+
 export default function Page() {
-  return <div>修改密码</div>;
+  return (
+    <Card title="修改密码">
+      <Form
+        labelCol={{
+          span: 5,
+        }}
+        labelAlign="left"
+        autoComplete="off"
+        style={{ maxWidth: 440 }}
+      >
+        <Form.Item label="原密码" name="password" rules={[{ required: true }]}>
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="新密码"
+          name="newpassword"
+          rules={[{ required: true }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="确认新密码"
+          name="checkPassword"
+          rules={[{ required: true }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          wrapperCol={{
+            offset: 5,
+          }}
+        >
+          <Button type="primary" htmlType="submit" block>
+            提交
+          </Button>
+        </Form.Item>
+      </Form>
+    </Card>
+  );
 }
