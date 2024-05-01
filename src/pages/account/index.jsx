fcb29d626f146e1e9f8619b828aa567f3be191d8
caihai <ch15519060282@163.com>
2024-05-02 04:16:20 +0800

    【样式】调整布局

--- src/pages/account/index.jsx
+++ src/pages/account/index.jsx
@@ -1,4 +1,14 @@
-import { Form, Avatar, Space, Button, Input, Row, Col, TreeSelect } from "antd";
+import {
+  Card,
+  Form,
+  Avatar,
+  Space,
+  Button,
+  Input,
+  Row,
+  Col,
+  TreeSelect,
+} from "antd";
 import DictSelect from "@/components/DictSelect";
 import { useGetDeptSelectQuery } from "@/store/api/deptSlice";
 
@@ -6,35 +16,37 @@ export default function Page() {
   const { data: depeOptions } = useGetDeptSelectQuery();
 
   return (
-    <Form
-      layout="vertical"
-      autoComplete="off"
-      initialValues={{
-        account: "admin",
-        name: "Cai Hai",
-        gender: "1",
-        phone: "18888888888",
-        email: "12345678@email.com",
-        deptId: ["1", "2"],
-      }}
-    >
-      <div>
-        <h2>我的头像</h2>
-        <Form.Item name="avatar">
-          <Space>
-            <Avatar
-              size={100}
-              src="https://avatars.githubusercontent.com/u/47770861?v=4"
-            />
-            <Button>上传</Button>
-          </Space>
-        </Form.Item>
-      </div>
-
-      <div>
-        <h2>基础信息</h2>
+    <Card title="基础信息">
+      <Form
+        layout="vertical"
+        autoComplete="off"
+        initialValues={{
+          account: "admin",
+          name: "Cai Hai",
+          gender: "1",
+          phone: "18888888888",
+          email: "12345678@email.com",
+          deptId: ["1", "2"],
+        }}
+      >
         <Row gutter={16}>
-          <Col span={8}>
+          <Col span={24} xxl={8}>
+            <Form.Item name="avatar">
+              <Space>
+                <Avatar
+                  size={166}
+                  src="https://avatars.githubusercontent.com/u/47770861?v=4"
+                />
+                <Button>上传</Button>
+              </Space>
+            </Form.Item>
+          </Col>
+          <Col span={24} xxl={16}>
+            <Form.Item label="个人简介">
+              <Input.TextArea showCount maxLength={100} placeholder="请输入" />
+            </Form.Item>
+          </Col>
+          <Col span={12} xxl={8}>
             <Form.Item
               label="用户名/账号"
               name="account"
@@ -43,7 +55,7 @@ export default function Page() {
               <Input placeholder="请输入" />
             </Form.Item>
           </Col>
-          <Col span={8}>
+          <Col span={12} xxl={8}>
             <Form.Item
               label="真实姓名"
               name="name"
@@ -52,17 +64,26 @@ export default function Page() {
               <Input placeholder="请输入" />
             </Form.Item>
           </Col>
-          <Col span={8}>
+          <Col span={12} xxl={8}>
+            <Form.Item
+              label="所属部门"
+              name="deptId"
+              rules={[{ required: true }]}
+            >
+              <TreeSelect treeData={depeOptions} disabled multiple></TreeSelect>
+            </Form.Item>
+          </Col>
+          <Col span={12} xxl={8}>
             <Form.Item label="性别" name="gender" rules={[{ required: true }]}>
               <DictSelect dictName="gender" />
             </Form.Item>
           </Col>
-          <Col span={8}>
+          <Col span={12} xxl={8}>
             <Form.Item label="手机号" name="phone" rules={[{ required: true }]}>
               <Input placeholder="请输入" />
             </Form.Item>
           </Col>
-          <Col span={8}>
+          <Col span={12} xxl={8}>
             <Form.Item
               label="电子邮件"
               name="email"
@@ -71,28 +92,12 @@ export default function Page() {
               <Input placeholder="请输入" />
             </Form.Item>
           </Col>
-          <Col span={16}>
-            <Form.Item label="个人简介">
-              <Input.TextArea placeholder="请输入" />
-            </Form.Item>
-          </Col>
         </Row>
-      </div>
 
-      <div>
-        <h2>所属部门</h2>
-        <Row gutter={16}>
-          <Col span={8}>
-            <Form.Item name="deptId" rules={[{ required: true }]}>
-              <TreeSelect treeData={depeOptions} disabled multiple></TreeSelect>
-            </Form.Item>
-          </Col>
-        </Row>
-      </div>
-
-      <Button type="primary" htmlType="submit">
-        保存更改
-      </Button>
-    </Form>
+        <Button type="primary" htmlType="submit">
+          保存更改
+        </Button>
+      </Form>
+    </Card>
   );
 }
