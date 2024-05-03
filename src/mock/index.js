e438c3cfb9113874279983369cfd9c48416e4357
caihai <ch15519060282@163.com>
2024-05-03 20:33:34 +0800

    【删除】此设计下不需要菜单管理和角色管理

--- src/mock/index.js
+++ src/mock/index.js
@@ -1,17 +1,15 @@
 import Mock from "mockjs";
 
 import login from "./login";
-import role from "./premis/role";
 import account from "./premis/account";
 import dept from "./premis/dept";
 import issues from "./issues";
-import menu from "./premis/menu";
 
 Mock.setup({
   timeout: "50-1000",
 });
 
-const mock = [...login, ...role, ...account, ...dept, ...issues, ...menu];
+const mock = [...login, ...account, ...dept, ...issues];
 
 mock.forEach(({ url, type, handler }) => {
   Mock.mock(url, type, function (...params) {
