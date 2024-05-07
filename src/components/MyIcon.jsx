99c5a100fee7b914e6a5622c3d8b2eb12b5dc7fc
caihai <ch15519060282@163.com>
2024-05-07 21:04:51 +0800

    【新增】新增自定义图标组件

---
+++ src/components/MyIcon.jsx
@@ -0,0 +1,7 @@
+import { createFromIconfontCN } from "@ant-design/icons";
+
+const MyIcon = createFromIconfontCN({
+  scriptUrl: "//at.alicdn.com/t/c/font_4536594_tzwqdxvrmzn.js", // 在 iconfont.cn 上生成
+});
+
+export default MyIcon;
