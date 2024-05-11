c40cd8a7ec7f269f193eac48f250115dcb4941c9
caihai <ch15519060282@163.com>
2024-05-11 23:57:03 +0800

    【优化】使用ModalForm优化代码

--- src/pages/system/role/AddOrEdit.tsx
+++ src/pages/system/role/AddOrEdit.tsx
@@ -1,8 +1,7 @@
-import { useState, forwardRef, useImperativeHandle } from "react";
-import { Modal, Space, Button, Form, Input, App } from "antd";
-import useLoadingDelayAndKeep from "@/hooks/useLoadingDelayAndKeep";
+import { useRef, useState, forwardRef, useImperativeHandle } from "react";
+import { Form, Input, App } from "antd";
 import axios from "@/utils/axios";
-
+import ModalForm, { type Ref as ModalFormRef } from "@/components/ModalForm";
 import { Role } from "@/api/role";
 
 export type Props = {
@@ -16,78 +15,52 @@ export type Ref = {
 const AddOrEdit = forwardRef<Ref, Props>((props, ref) => {
   const { message } = App.useApp();
 
-  const [headForm] = Form.useForm();
+  const modalFormRef = useRef<ModalFormRef>(null);
 
   const [id, setId] = useState("");
-  const [visible, setVisible] = useState(false);
-  const [loading, { setTrue, setFalse }] = useLoadingDelayAndKeep(false);
 
   const onSubmit = function (data: Pick<Role, "roleName" | "description">) {
-    setTrue();
     const apiUrl = id ? "/api/role/update" : "/api/role/add";
     const successMsg = id ? "更新成功！" : "新增成功！";
     const params = id ? { id, ...data } : data;
     const callback = () => props.callback?.(id ? undefined : 1);
-    axios
-      .post(apiUrl, params)
-      .then(() => {
-        setVisible(false);
-        message.success(successMsg);
-        callback();
-      })
-      .finally(() => setFalse());
+    return axios.post(apiUrl, params).then(() => {
+      message.success(successMsg);
+      callback();
+    });
   };
 
   useImperativeHandle(ref, () => ({
-    addStart: () => setVisible(true),
+    addStart: () => modalFormRef.current?.open(),
     editStart: (row) => {
       setId(row.id);
-      headForm.setFieldsValue(row);
-      setVisible(true);
+      modalFormRef.current?.formInstance.setFieldsValue(row);
+      modalFormRef.current?.open();
     },
   }));
 
   return (
-    <Modal
+    <ModalForm
+      ref={modalFormRef}
       title={id ? "编辑" : "新增"}
-      open={visible}
       width={600}
-      footer={
-        <Space>
-          <Button onClick={() => setVisible(false)}>取消</Button>
-          <Button
-            type="primary"
-            loading={loading}
-            onClick={() => headForm.submit()}
-          >
-            确定
-          </Button>
-        </Space>
-      }
-      onCancel={() => setVisible(false)}
       afterClose={() => {
         setId("");
-        headForm.resetFields();
+        modalFormRef.current?.formInstance.resetFields();
       }}
+      onFinish={onSubmit}
     >
-      <Form
-        form={headForm}
-        layout="vertical"
-        onFinish={onSubmit}
-        autoComplete="off"
+      <Form.Item
+        name="roleName"
+        label="角色名称"
+        rules={[{ required: true, message: "请填写角色名称" }]}
       >
-        <Form.Item
-          name="roleName"
-          label="角色名称"
-          rules={[{ required: true, message: "请填写角色名称" }]}
-        >
-          <Input placeholder="请输入" />
-        </Form.Item>
-        <Form.Item name="description" label="备注">
-          <Input.TextArea placeholder="请输入" />
-        </Form.Item>
-      </Form>
-    </Modal>
+        <Input placeholder="请输入" />
+      </Form.Item>
+      <Form.Item name="description" label="备注">
+        <Input.TextArea placeholder="请输入" />
+      </Form.Item>
+    </ModalForm>
   );
 });
 
