db496987aa2b593dc5d384bb01fa6707745686f6
caihai <ch15519060282@163.com>
2024-05-19 18:11:22 +0800

    【优化】优化类型文件

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -53,9 +53,12 @@ export const deepCopy = function <T extends any>(obj: T): T {
 
 // ================================ 分割线 ================================
 
-type TreeNode = {
-  [key: string]: any;
-  children?: TreeNode[];
+// 用来阻断类型推断
+type NoInfer<T> = [T][T extends any ? 0 : never];
+
+type TreeNode<A extends any, T extends string = "children"> = A & {
+  // eslint-disable-next-line no-unused-vars
+  [key in T]?: TreeNode<A, T>[];
 };
 
 /**
@@ -64,40 +67,52 @@ type TreeNode = {
  *
  * @param treeData - 需要过滤的树形结构数据。
  * @param predicate - 用于判断节点是否保留的函数。
+ * @param children - 子节点属性的名称，默认为 'children'。
  * @returns 过滤后的新树形结构。
  */
-export const treeFilter = function (
+export const treeFilter = function <
+  A extends any,
+  T extends string = "children"
+>(
   /** 要过滤的树结构 */
-  treeData: TreeNode[] = [],
+  treeData: TreeNode<A, NoInfer<T>>[] = [],
   /** 该函数用于确定是否应保留节点 */
-  predicate: (node: TreeNode) => boolean
-): TreeNode[] {
+  predicate: (node: TreeNode<A, NoInfer<T>>) => boolean,
+  /** 子节点属性的名称 */
+  children?: T
+): TreeNode<A, NoInfer<T>>[] {
   // 深拷贝树形数据
   const treeDataCopy = deepCopy(treeData);
 
   // 递归过滤函数
-  const filterTree = (nodes: TreeNode[]): TreeNode[] => {
+  const filterTree = (
+    nodes: TreeNode<A, NoInfer<T>>[]
+  ): TreeNode<A, NoInfer<T>>[] => {
+    const _childrenKey = children || "children";
+
     return nodes
       .map((node) => {
         // 递归过滤子节点
-        const children = filterTree(node.children || []);
+        const children = filterTree(node[_childrenKey] || []);
 
         // 检查当前节点是否通过过滤函数
         if (predicate(node)) {
           // 如果节点通过过滤，保留其过滤后的子节点（如果有的话）并返回节点
-          node.children = children.length > 0 ? children : undefined;
+          if (children && children.length > 0) {
+            (node[_childrenKey] as TreeNode<A, NoInfer<T>>[]) = children;
+          }
           return node;
         } else {
           // 如果节点没有通过过滤，检查其子节点是否有通过过滤的
           if (children && children.length > 0) {
-            node.children = children;
+            (node[_childrenKey] as TreeNode<A, NoInfer<T>>[]) = children;
             return node;
           }
           // 如果节点和其子节点都没有通过过滤，返回 null
           return null;
         }
       })
-      .filter((node) => node !== null) as TreeNode[]; // 过滤掉返回 null 的节点
+      .filter((node) => node !== null) as TreeNode<A, NoInfer<T>>[]; // 过滤掉返回 null 的节点
   };
 
   return filterTree(treeDataCopy);
@@ -111,21 +126,34 @@ export const treeFilter = function (
  *
  * @param treeData - 要遍历和转换的树形结构数据。
  * @param callback - 应用于每个节点的回调函数。
+ * @param childrenKey - 子节点属性的名称，默认为 'children'。
+ * @param newChildrenKey - map过后子节点的属性名称，默认为 'children'。
  * @returns 新的树形结构，节点经过回调函数的转换。
  */
-export const treeMap = function (
-  treeData: TreeNode[] = [],
-  callback: (node: TreeNode) => TreeNode
-): TreeNode[] {
+export const treeMap = function <
+  A extends any,
+  T extends string = "children",
+  N extends string = "children"
+>(
+  treeData: TreeNode<A, NoInfer<T>>[] = [],
+  callback: (node: TreeNode<A, NoInfer<T>>) => TreeNode<any, N>,
+  children?: T,
+  newChildren?: N
+): TreeNode<any, N>[] {
+  const _childrenKey = children || "children";
+  const _newChildrenKey = newChildren || "children";
+
   // 递归遍历和转换树的函数
-  const mapTree = (nodes: TreeNode[]): TreeNode[] => {
+  const mapTree = (nodes: TreeNode<A, T>[]): TreeNode<any, N>[] => {
     return nodes.map((node) => {
       // 对当前节点应用回调函数
       const newNode = callback({ ...node });
 
+      const children = node[_childrenKey] as TreeNode<A, T>[];
+
       // 如果有子节点，递归应用回调函数
-      if (newNode.children && newNode.children.length > 0) {
-        newNode.children = mapTree(newNode.children);
+      if (children && children.length > 0) {
+        newNode[_newChildrenKey] = mapTree(children);
       }
 
       return newNode;
