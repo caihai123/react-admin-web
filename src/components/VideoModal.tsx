0a53ec4fcdcc96dfe3497f3a21c92bf590015251
caihai <ch15519060282@163.com>
2024-05-29 12:37:58 +0800

    【优化】处理视频播放失败时

--- src/components/VideoModal.tsx
+++ src/components/VideoModal.tsx
@@ -1,4 +1,5 @@
-import { Modal } from "antd";
+import { useState } from "react";
+import { Modal, Result, theme } from "antd";
 import ReactDOM from "react-dom/client";
 import createCompoundedComponent from "@/components/utils/createCompoundedComponent";
 
@@ -9,10 +10,17 @@ type Props = {
   width?: number | string;
   height?: number | string;
   autoPlay?: boolean;
+  afterClose?: () => void;
 };
 
 const VideoModal = function (props: Props) {
   const { width = 800, height, autoPlay = true } = props;
+
+  const [error, setError] = useState<boolean>(false);
+
+  const {
+    token: { colorBgLayout },
+  } = theme.useToken();
 
   return (
     <Modal
@@ -22,22 +30,36 @@ const VideoModal = function (props: Props) {
       centered
       width={width}
       closeIcon={false}
+      afterClose={props.afterClose}
     >
-      <video
-        width="100%"
-        height={height}
-        autoPlay={autoPlay}
-        controls
-        src={props.url}
-      ></video>
+      {error ? (
+        <Result
+          status="error"
+          title="视频不能正常播放！"
+          subTitle="The video doesn't play properly!"
+          style={{ background: colorBgLayout }}
+        ></Result>
+      ) : (
+        <video
+          width="100%"
+          height={height}
+          autoPlay={autoPlay}
+          controls
+          src={props.url}
+          onError={() => setError(true)}
+          onPlay={() => setError(false)}
+        ></video>
+      )}
     </Modal>
   );
 };
 
 let root: ReactDOM.Root | null;
+let uuid: number = 1;
 
 /** 函数式调用 会挂载到body下 重复调用时只会保留最后一个*/
 const showModal = function (src: Props["url"]) {
+  uuid++;
   if (!root) {
     const container = document.createDocumentFragment();
     document.body.appendChild(container);
@@ -45,11 +67,16 @@ const showModal = function (src: Props["url"]) {
   }
 
   const handleClose = () => {
-    root?.render(<VideoModal url={src} visible={false} />);
+    root?.render(<VideoModal key={uuid} url={src} visible={false} />);
   };
 
   root.render(
-    <VideoModal url={src} visible={true} onClose={() => handleClose()} />
+    <VideoModal
+      url={src}
+      key={uuid}
+      visible={true}
+      onClose={() => handleClose()}
+    />
   );
 };
 
