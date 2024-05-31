ad57ab45dd5880ddfdd3c7d13a79dcdd0f2140f3
caihai <ch15519060282@163.com>
2024-05-31 23:51:11 +0800

    【新增】添加animate动画

--- src/pages/Transition.tsx
+++ src/pages/Transition.tsx
@@ -4,6 +4,7 @@ import { useBoolean } from "ahooks";
 import { CSSTransition } from "react-transition-group";
 import CollapseTransition from "@/components/CollapseTransition";
 import "@/styles/transition.css";
+import "animate.css";
 
 export default function AnimateDemo() {
   const {
@@ -13,6 +14,7 @@ export default function AnimateDemo() {
   const [show1, { toggle: toggle1 }] = useBoolean(true);
   const [show2, { toggle: toggle2 }] = useBoolean(true);
   const [show3, { toggle: toggle3 }] = useBoolean(true);
+  const [show4, { toggle: toggle4 }] = useBoolean(true);
 
   const nodeRef1 = React.useRef(null);
   const nodeRef2 = React.useRef(null);
@@ -21,6 +23,10 @@ export default function AnimateDemo() {
   const nodeRef5 = React.useRef(null);
   const nodeRef6 = React.useRef(null);
   const nodeRef7 = React.useRef(null);
+  const nodeRef8 = React.useRef(null);
+  const nodeRef9 = React.useRef(null);
+  const nodeRef10 = React.useRef(null);
+  const nodeRef11 = React.useRef(null);
 
   const transitionBoxStyle: React.CSSProperties = {
     width: 200,
@@ -155,6 +161,103 @@ export default function AnimateDemo() {
                 collapse-transition-horizontal
               </div>
             </CollapseTransition>
+          </div>
+        </Card>
+      </div>
+
+      <div style={{ marginTop: 20 }}>
+        <h2>Animate 动画</h2>
+        <p>
+          animate.css 确实太花哨了一点，不过它和 CollapseTransition
+          配合起来真的太香了，而且还包含了上面的 fade 和 zoom，甚至更花，
+          以下是几个案例
+        </p>
+        <Card>
+          <Button onClick={() => toggle4()}>点我</Button>
+          <div style={{ display: "flex", marginTop: 20, height: 100 }}>
+            <CSSTransition
+              in={show4}
+              nodeRef={nodeRef8}
+              timeout={1000}
+              unmountOnExit
+              appear
+              classNames={{
+                appear: "animate__backInDown",
+                enter: "animate__backInDown",
+                exit: "animate__backOutDown",
+              }}
+            >
+              <div
+                ref={nodeRef8}
+                className="animate__animated"
+                style={transitionBoxStyle}
+              >
+                backInDown and backOutDown
+              </div>
+            </CSSTransition>
+
+            <CSSTransition
+              in={show4}
+              nodeRef={nodeRef9}
+              timeout={1000}
+              unmountOnExit
+              appear
+              classNames={{
+                appear: "animate__backInLeft",
+                enter: "animate__backInLeft",
+                exit: "animate__backOutLeft",
+              }}
+            >
+              <div
+                ref={nodeRef9}
+                className="animate__animated"
+                style={transitionBoxStyle}
+              >
+                backInLeft and backOutLeft
+              </div>
+            </CSSTransition>
+
+            <CSSTransition
+              in={show4}
+              nodeRef={nodeRef10}
+              timeout={1000}
+              unmountOnExit
+              appear
+              classNames={{
+                appear: "animate__backInRight",
+                enter: "animate__backInRight",
+                exit: "animate__backOutRight",
+              }}
+            >
+              <div
+                ref={nodeRef10}
+                className="animate__animated"
+                style={transitionBoxStyle}
+              >
+                backInRight and backOutRight
+              </div>
+            </CSSTransition>
+
+            <CSSTransition
+              in={show4}
+              nodeRef={nodeRef11}
+              timeout={1000}
+              unmountOnExit
+              appear
+              classNames={{
+                appear: "animate__backInUp",
+                enter: "animate__backInUp",
+                exit: "animate__backOutUp",
+              }}
+            >
+              <div
+                ref={nodeRef11}
+                className="animate__animated"
+                style={transitionBoxStyle}
+              >
+                backInUp or backOutUp
+              </div>
+            </CSSTransition>
           </div>
         </Card>
       </div>
