aec0f2debf9a699a6118c9bda74e7a1effc2f774
caihai <ch15519060282@163.com>
2024-06-06 16:55:28 +0800

    【优化】只在屏幕宽度触发界线时才更新collapsed

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { Layout } from "antd";
 import { useMount, useLocalStorageState } from "ahooks";
 import { useDispatch } from "react-redux";
@@ -19,10 +19,19 @@ export default function LayoutViwe() {
   const dispatch = useDispatch<AppDispatch>();
   useMount(() => dispatch(initMenu()));
 
+  const oldScreenWidth = useRef<number>(document.body.clientWidth);
   // 根据屏幕宽度自动设置菜单栏展开状态
   const resizeEnet = () => {
+    const minWidth = 1200;
     const screenWidth = document.body.clientWidth;
-    setCollapsed(screenWidth < 1200);
+    if (oldScreenWidth.current >= minWidth && screenWidth < minWidth) {
+      // 从大屏切换到小屏
+      setCollapsed(true);
+    } else if (oldScreenWidth.current < minWidth && screenWidth >= minWidth) {
+      // 从小屏切换到大屏
+      setCollapsed(false);
+    }
+    oldScreenWidth.current = screenWidth;
   };
   useEffect(() => {
     // 为什么不使用 useDomSize? 因为 Layout 的高度经常都可能改变，比如页面切换的时候，使用 useDomSize 会很频繁的触发。
