574f8e46c89e92d14dcccb0a36b1dba2d3e349f1
caihai <ch15519060282@163.com>
2024-06-08 14:55:12 +0800

    【修复】title过长时省略

--- src/layout/Logo.tsx
+++ src/layout/Logo.tsx
@@ -1,26 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import LogoSvg from "@/assets/logo.svg";
-import styled, { keyframes } from "styled-components";
-
-const titleHide = keyframes`
-    0% {
-      display: none;
-      opacity: 0;
-    }
-
-    80% {
-      display: none;
-      opacity: 0;
-    }
-
-    to {
-      display: unset;
-      opacity: 1;
-    }
-  `;
+import styled from "styled-components";
+import useContentOverflow from "@/hooks/useContentOverflow";
 
 const LogoStyle = styled.div`
+  width: 100%;
   position: relative;
   display: flex;
   align-items: center;
@@ -28,13 +13,10 @@ const LogoStyle = styled.div`
   cursor: pointer;
   transition: padding 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);
   & > a {
-    display: flex;
-    align-items: center;
-    justify-content: center;
+    width: 100%;
     min-height: 32px;
   }
   & img {
-    display: inline-block;
     height: 32px;
     vertical-align: middle;
   }
@@ -45,9 +27,10 @@ const LogoStyle = styled.div`
     font-size: 18px;
     line-height: 32px;
     vertical-align: middle;
-    animation: ${titleHide} 0.3s;
   }
 `;
+
+const title = process.env.REACT_APP_WEBSITE_NAME;
 
 export default function Logo(props: {
   // title: React.ReactNode;
@@ -57,17 +40,18 @@ export default function Logo(props: {
 }) {
   const { collapsed = false, logo = LogoSvg } = props;
 
+  const linkRef = React.useRef(null);
+  const { isOverflowX } = useContentOverflow(linkRef);
+
   return (
     <LogoStyle style={props.style}>
-      <Link to="/">
+      <Link to="/" className="ellipsis" ref={linkRef}>
         <img src={logo} alt="logo" />
         <h1
-          className="ellipsis"
-          style={{
-            display: collapsed ? "none" : "block",
-          }}
+          title={isOverflowX ? title : undefined}
+          style={{ display: collapsed ? "none" : "inline" }}
         >
-          {process.env.REACT_APP_WEBSITE_NAME}
+          {title}
         </h1>
       </Link>
     </LogoStyle>
